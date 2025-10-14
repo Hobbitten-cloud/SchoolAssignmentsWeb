3d07acb3b3eb9c5615a2ea9e7a3bf83e93960b46
Hobbitten-cloud <janenhansen@gmail.com>
2025-10-14 09:46:29 +0200

    forward frontend

---
+++ AssignmentsWeb/Controllers/AssignmentController.cs
@@ -0,0 +1,16 @@
+﻿
+using AssignmentsWeb.Models.Domain;
+using AssignmentsWeb.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentsWeb.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var testRepo = new TestRepo();
+            return View(testRepo.GetAll());
+        }
+    }
+}
---
+++ AssignmentsWeb/Persistence/TestRepo.cs
@@ -0,0 +1,43 @@
+﻿using AssignmentsWeb.Models.Domain;
+using AssignmentsWeb.Models.Enums;
+
+namespace AssignmentsWeb.Persistence
+{
+    public class TestRepo
+    {
+        private List<Assignment> _assignments = new List<Assignment>
+        {
+            new Assignment
+            {
+                Id = 1,
+                Title = "Opgave 1",
+                Subjects = Subjects.Programming,
+                Description = "Du skal lære om enums"
+            },
+            new Assignment
+            {
+                Id = 2,
+                Title = "Opgave 2",
+                Subjects = Subjects.Systemdevelopment,
+                Description = "Du skal nu lære om hvad en systemudviklings metode er"
+            },
+            new Assignment
+            {
+                Id = 2,
+                Title = "Opgave 3",
+                Subjects = Subjects.Technology,
+                Description = "Vi skal lære om binare tal"
+            }
+        };
+
+        public List<Assignment> GetAll()
+        {
+            foreach (var assignment in _assignments)
+            {
+
+            }
+
+            return _assignments;
+        }
+    }
+}
