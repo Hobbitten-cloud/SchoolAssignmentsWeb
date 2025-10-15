6db0c3ca8115c47d2a3ddb394703b371afe073ca
Hobbitten-cloud <janenhansen@gmail.com>
2025-10-15 16:02:08 +0200

    Added a bunch of bootstrap stuff + updated some UI on some important views

---
+++ AssignmentsWeb/Controllers/FunWithUIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentsWeb.Controllers
+{
+    public class FunWithUIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Spinner()
+        {
+            return View();
+        }
+
+        public IActionResult CarouselSlide()
+        {
+            return View();
+        }
+
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+    }
+}
