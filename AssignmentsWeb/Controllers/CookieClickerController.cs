93376c5bfe7de479836910f5025e1f6d1b6223f0
Hobbitten-cloud <janenhansen@gmail.com>
2025-10-17 23:07:56 +0200

    Added a fun little game "cookie clicker" from someone elses html and javascript code to a view

---
+++ AssignmentsWeb/Controllers/CookieClickerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentsWeb.Controllers
+{
+    public class CookieClickerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
